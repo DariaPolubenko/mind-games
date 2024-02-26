0eca7cb004f1bd4ad0d71d00b92d75df8d71093e
Daria Polubenko <daria.polubenko@gmail.com>
2024-02-27 05:40:36 +0700

    update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,11 @@ plugins {
     id("java")
     id("se.patrikerdes.use-latest-versions") version "0.2.18"
     id("com.github.ben-manes.versions") version "0.51.0"
+    application
+}
+
+application {
+    mainClass = "hexlet.code.App"
 }
 
 group = "hexlet.code"
@@ -14,7 +19,8 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.2"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    runtimeOnly("com.jetbrains.intellij.packageSearch:package-search:231.9414.13")
+    testImplementation("com.jetbrains.intellij.packageSearch:package-search:231.9414.13")
+    testImplementation("io.ktor:ktor-client-cio:2.3.6")
 }
 
 tasks.test {
