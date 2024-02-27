143b924e2f22111512078fb774f9580b7245316b
Daria Polubenko <daria.polubenko@gmail.com>
2024-02-28 03:55:03 +0700

    add list of game in App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,21 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        Cli.getName();
+
+        Scanner choice = new Scanner(System.in);
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet\n" + "0 - Exit");
+
+        int userChoice = choice.nextInt();
+        System.out.println("Your choice: " + userChoice + "\n");
+        //userChoice.close();
+
+        if (userChoice == 1) {
+            System.out.println("Welcome to the Brain Games!");
+            Cli.getName();
+        }
     }
 }
