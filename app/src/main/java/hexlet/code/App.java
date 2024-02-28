d2b69020ff9691e9ae92882bb2d87fa80898e32a
Daria Polubenko <daria.polubenko@gmail.com>
2024-02-28 22:21:23 +0700

    update App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -6,16 +6,26 @@ class App {
     public static void main(String[] args) {
 
         Scanner choice = new Scanner(System.in);
-        System.out.println("Please enter the game number and press Enter.");
-        System.out.println("1 - Greet\n" + "0 - Exit");
+        System.out.println("Please enter the game number and press Enter.\n"
+                + "1 - Greet\n"
+                + "2 - Even\n"
+                + "0 - Exit");
 
         int userChoice = choice.nextInt();
         System.out.println("Your choice: " + userChoice + "\n");
         //userChoice.close();
 
-        if (userChoice == 1) {
+        if (userChoice >= 1) {
             System.out.println("Welcome to the Brain Games!");
-            Cli.getName();
+            var userName = Cli.getName();
+            System.out.println("Hello, " + userName + "!");
+
+                switch (userChoice) {
+                    case 2:
+                        var resultEven = Even.getEven();
+                        System.out.println(resultEven + userName);
+                        //case 3:
+                }
         }
     }
 }
