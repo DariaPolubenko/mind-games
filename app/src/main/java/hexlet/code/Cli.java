28c2756ebdc1a63d3aec6cac698ee99c128835e6
Daria Polubenko <daria.polubenko@gmail.com>
2024-02-28 22:21:47 +0700

    update Cli.java

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,13 +3,12 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-    public static void getName() {
+    public static String getName() {
         Scanner name = new Scanner(System.in);
 
         System.out.print("May I have your name? \n");
         String userName = name.next();
-        System.out.println("Hello, " + userName + "!");
 
-        name.close();
+        return userName;
     }
 }
