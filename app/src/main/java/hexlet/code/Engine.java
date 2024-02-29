1d2a2efec4e9af766b767ad5ff4a082252ca79b4
Daria Polubenko <daria.polubenko@gmail.com>
2024-02-29 22:03:54 +0700

    add Engine.java

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -1,2 +1,21 @@
-package hexlet.code;public class Engine {
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Engine {
+    public static boolean getEngine(String result) {
+
+        //System.out.println(question);
+        Scanner answer = new Scanner(System.in);
+        System.out.print("Your answer: ");
+        String userAnswer = answer.next();
+
+        if (result.equals(userAnswer)) {
+            System.out.println("Correct!");
+        } else if (!result.equals(userAnswer)) {
+            System.out.println("'" + userAnswer + "'" + " is wrong answer ;(. Correct answer was " + "'" + result + "'.");
+            return false;
+        }
+        return true;
+    }
 }
