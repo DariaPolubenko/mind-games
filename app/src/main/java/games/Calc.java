7f0d93a0b1834d7ac2dde594167befe3d2bee74f
Daria Polubenko <daria.polubenko@gmail.com>
2024-03-02 07:41:32 +0700

    update Calc.java

--- app/src/main/java/games/Calc.java
+++ app/src/main/java/games/Calc.java
@@ -2,47 +2,52 @@ package games;
 
 import hexlet.code.Engine;
 
-
 public class Calc {
-    public static String getCalc() {
+    public static void getCalc() {
 
-        System.out.println("What is the result of the expression?");
+        var greeting = "What is the result of the expression?";
 
-        for (var i = 1; i <= 3; i++) {
-            var firstNumber = (int) (Math.random() * 100);
-            var secondNumber = (int) (Math.random() * 100);
+        int[][] numbers = new int[3][2];
+        String[] questions = new String[3];
 
-            char[] operations = {'+', '-', '*'};
+        String[] operations = {"+", "-", "*"};
+
+        int[] results = new int[3];
+        String[] resultStrings = new String[3];
+
+
+        for (var i = 0; i < 3; i++) {
+
             int minId = 0;
             int maxId = 2;
             int randomOperation = minId + (int) (Math.random() * (maxId - minId + 1));
-
             var operation = operations[randomOperation];
-            System.out.println("Question: " + firstNumber + " " + operation + " " + secondNumber);
 
-            int result = 0;
+            if (operation.equals("*")) {
+                numbers[i][0] = (int) (Math.random() * 10);
+                numbers[i][1] = (int) (Math.random() * 100);
+            } else {
+                numbers[i][0] = (int) (Math.random() * 100);
+                numbers[i][1] = (int) (Math.random() * 100);
+            }
+
+            questions[i] = ("Question: " + numbers[i][0] + " " + operation + " " + numbers[i][1]);
+
             switch (operation) {
-                case '+':
-                    result = firstNumber + secondNumber;
+                case "+":
+                    results[i] = numbers[i][0] + numbers[i][1];
                     break;
 
-                case '-':
-                    result = firstNumber - secondNumber;
+                case "-":
+                    results[i] = numbers[i][0] - numbers[i][1];
                     break;
-                case '*':
-                    result = firstNumber * secondNumber;
+                case "*":
+                    results[i] = numbers[i][0] * numbers[i][1];
                     break;
             }
 
-            var resultString = String.valueOf(result);
-            var output = Engine.getEngine(resultString);
-
-            if (!output) {
-                return "Let's try again, ";
-            }
-
+            resultStrings[i] = String.valueOf(results[i]);
         }
-
-        return "Congratulations, ";
+        Engine.getEngine(greeting, questions, resultStrings);
     }
 }
