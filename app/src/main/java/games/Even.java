30d688e7c57477412df3e45197bdaa40ab17ed50
Daria Polubenko <daria.polubenko@gmail.com>
2024-03-04 04:53:20 +0700

    update Even.java

--- app/src/main/java/games/Even.java
+++ app/src/main/java/games/Even.java
@@ -6,17 +6,15 @@ public class Even {
     public static void getEven() {
 
         var greeting = "Answer 'yes' if the number is even, otherwise answer 'no'.";
-
-        int[] numbers = new int[3];
         String[] questions = new String[3];
         String[] results = new String[3];
 
         for (var i = 0; i < 3; i++) {
 
-            numbers[i] = (int) (Math.random() * 100);
-            questions[i] = ("Question: " + numbers[i]);
+            var number = (int) (Math.random() * 100);
+            questions[i] = "Question: " + number;
 
-            int even = numbers[i] % 2;
+            int even = number % 2;
             if (even == 0) {
                 results[i] = "yes";
             } else {
