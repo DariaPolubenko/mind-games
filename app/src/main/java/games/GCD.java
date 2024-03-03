fc3bc3c31b2cf907d7821b15d24da82250566f7b
Daria Polubenko <daria.polubenko@gmail.com>
2024-03-04 04:53:40 +0700

    update GCD.java

--- app/src/main/java/games/GCD.java
+++ app/src/main/java/games/GCD.java
@@ -6,36 +6,36 @@ public class GCD {
     public static void getGCD() {
 
         var greeting = "Find the greatest common divisor of given numbers.";
+        String[] questions = new String[3];
+        String[] results = new String[3];
 
-            int[][] numbers = new int[3][2];
-            String[] questions = new String[3];
-            String[] results = new String[3];
+        for (var i = 0; i < 3; i++) {
 
-            for (var i = 0; i < 3; i++) {
+            int[] numbers = new int[2];
 
-                int minMultiplier = 1;
-                int maxMultiplier = 10;
-                int multiplier = minMultiplier + (int) (Math.random() * (maxMultiplier - minMultiplier + 1));
+            int minMultiplier = 1;
+            int maxMultiplier = 10;
+            int multiplier = minMultiplier + (int) (Math.random() * (maxMultiplier - minMultiplier + 1));
 
-                do {
-                    numbers[i][0] = minMultiplier + (int) (Math.random() * (maxMultiplier - minMultiplier + 1));
-                    numbers[i][1] = minMultiplier + (int) (Math.random() * (maxMultiplier - minMultiplier + 1));
-                } while (numbers[i][0] == numbers[i][1]);
+            do {
+                numbers[0] = minMultiplier + (int) (Math.random() * (maxMultiplier - minMultiplier + 1));
+                numbers[1] = minMultiplier + (int) (Math.random() * (maxMultiplier - minMultiplier + 1));
+            } while (numbers[0] == numbers[1]);
 
-                numbers[i][0] = numbers[i][0] * multiplier;
-                numbers[i][1] = numbers[i][1] * multiplier;
+            numbers[0] = numbers[0] * multiplier;
+            numbers[1] = numbers[1] * multiplier;
 
-                questions[i] = ("Question: " + numbers[i][0] + " " + numbers[i][1]);
+            questions[i] = "Question: " + numbers[0] + " " + numbers[1];
 
-                while (numbers[i][0] != 0 && numbers[i][1] != 0) {
-                    if (numbers[i][0] > numbers[i][1]) {
-                        numbers[i][0] = numbers[i][0] % numbers[i][1];
+            while (numbers[0] != 0 && numbers[1] != 0) {
+                    if (numbers[0] > numbers[1]) {
+                        numbers[0] = numbers[0] % numbers[1];
                     } else {
-                        numbers[i][1] = numbers[i][1] % numbers[i][0];
+                        numbers[1] = numbers[1] % numbers[0];
                     }
                 }
 
-                var result = numbers[i][0] + numbers[i][1];
+                var result = numbers[0] + numbers[1];
                 results[i] = String.valueOf(result);
             }
 
