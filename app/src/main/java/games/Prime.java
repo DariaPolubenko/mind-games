eabeb929d6563bdc797b76ace67bbbfefaf8c38d
Daria Polubenko <daria.polubenko@gmail.com>
2024-03-04 04:53:59 +0700

    add Prime.java

--- app/src/main/java/games/Prime.java
+++ app/src/main/java/games/Prime.java
@@ -1,2 +1,39 @@
-package games;public class Prime {
+package games;
+
+import hexlet.code.Engine;
+
+public class Prime {
+    public static void getPrime() {
+
+        var greeting = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
+        String[] questions = new String[3];
+        String[] results = new String[3];
+
+        for (var i = 0; i < 3; i++) {
+
+            int number;
+            do {
+                number = (int) (Math.random() * 100);
+            } while (number == 0);
+
+            questions[i] = "Question: " + number;
+
+            boolean simpleNumber = true;
+
+            var splitter = 2;
+            while ((number > 1) && (splitter < 8)) {
+                if (number % splitter != 0) {
+                    simpleNumber = true;
+                    splitter += 1;
+            } else {
+                    simpleNumber = false;
+                    break;
+                }
+            }
+
+        results[i] = simpleNumber ? "yes" : "no";
+        }
+
+        Engine.getEngine(greeting, questions, results);
+    }
 }
