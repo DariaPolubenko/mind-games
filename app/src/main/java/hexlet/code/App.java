0cea9027a335a7005aabc021841101b98f245d85
Daria Polubenko <daria.polubenko@gmail.com>
2024-03-05 00:31:02 +0700

    update App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -12,7 +12,8 @@ class App {
     public static void main(String[] args) {
 
         Scanner choice = new Scanner(System.in);
-        System.out.println("Please enter the game number and press Enter.\n"
+        System.out.println("Please enter the game number and press Enter."
+                + "\n"
                 + "1 - Greet\n"
                 + "2 - Even\n"
                 + "3 - Calc\n"
