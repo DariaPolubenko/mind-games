038886f315e1a23ddf605c3823312a0297768956
Daria Polubenko <daria.polubenko@gmail.com>
2024-03-05 04:10:43 +0700

    update App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,17 +1,16 @@
 package hexlet.code;
 
-import games.Even;
 import games.Calc;
+import games.Even;
 import games.GCD;
-import games.Progression;
 import games.Prime;
+import games.Progression;
 
 import java.util.Scanner;
 
 class App {
     public static void main(String[] args) {
 
-        Scanner choice = new Scanner(System.in);
         System.out.println("Please enter the game number and press Enter.\n"
                 + "1 - Greet\n"
                 + "2 - Even\n"
@@ -21,34 +20,32 @@ class App {
                 + "6 - Prime\n"
                 + "0 - Exit");
 
-        int userChoice = choice.nextInt();
+        Scanner choice = new Scanner(System.in);
+        String userChoice = choice.next();
+
         System.out.println("Your choice: " + userChoice + "\n");
 
-        if (userChoice > 0) {
-            System.out.println("Welcome to the Brain Games!");
-
-            var userName = Cli.getName();
-            System.out.println("Hello, " + userName + "!");
-
-            switch (userChoice) {
-                case 1:
-                    break;
-                case 2:
-                    Even.getEven();
-                    break;
-                case 3:
-                    Calc.getCalc();
-                    break;
-                case 4:
-                    GCD.getGCD();
-                    break;
-                case 5:
-                    Progression.getProgression();
-                    break;
-                default:
-                    Prime.getPrime();
-                    break;
-            }
+        switch (userChoice) {
+            case "1":
+                Cli.getName();
+                break;
+            case "2":
+                Even.getEven();
+                break;
+            case "3":
+                Calc.getCalc();
+                break;
+            case "4":
+                GCD.getGCD();
+                break;
+            case "5":
+                Progression.getProgression();
+                break;
+            case "6":
+                Prime.getPrime();
+                break;
+            default:
+                break;
         }
     }
 }
