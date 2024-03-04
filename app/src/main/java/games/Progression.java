3083afd8f9ba30335147fcb745df8e16db310980
Daria Polubenko <daria.polubenko@gmail.com>
2024-03-05 00:06:39 +0700

    update Progression.java

--- app/src/main/java/games/Progression.java
+++ app/src/main/java/games/Progression.java
@@ -27,12 +27,12 @@ public class Progression {
             int maxMissing = length - 1;
             int missingNumber = (int) (Math.random() * maxMissing);
 
-            var question = new StringBuilder("Question: ");
+            var question = new StringBuilder("Question:");
             int result = 0;
 
             if (missingNumber == 0) {
                 result = numbers[0];
-                question.append("..");
+                question.append(" ..");
             }
 
             for (var j = 1; j < length; j++) {
