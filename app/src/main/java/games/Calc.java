3f4fb65997b80bbfe680342b4b6ec57dd886da94
Daria Polubenko <daria.polubenko@gmail.com>
2024-03-05 03:31:49 +0700

    update Calc.java

--- app/src/main/java/games/Calc.java
+++ app/src/main/java/games/Calc.java
@@ -6,26 +6,33 @@ public class Calc {
     public static void getCalc() {
 
         var greeting = "What is the result of the expression?";
-        String[] questions = new String[3];
-        String[] resultStrings = new String[3];
 
-        for (var i = 0; i < 3; i++) {
+        final int length = 3;
+        String[] questions = new String[length];
+        String[] resultStrings = new String[length];
 
-            int[] numbers = new int[2];
+        final int rounds = 3;
+        for (var i = 0; i < rounds; i++) {
+
+            final int numberOfDigits = 2;
+            int[] numbers = new int[numberOfDigits];
             String[] operations = {"+", "-", "*"};
             int result = 0;
 
-            int minElement = 0;
-            int maxElement = 2;
+            final int minElement = 0;
+            final int maxElement = 2;
             int randomOperation = minElement + (int) (Math.random() * (maxElement - minElement + 1));
             var operation = operations[randomOperation];
 
+            final int rangeOfNumbers1 = 100;
+            final int rangeOfNumbers2 = 10;
+
             if (operation.equals("*")) {
-                numbers[0] = (int) (Math.random() * 10);
-                numbers[1] = (int) (Math.random() * 100);
+                numbers[0] = (int) (Math.random() * rangeOfNumbers2);
+                numbers[1] = (int) (Math.random() * rangeOfNumbers1);
             } else {
-                numbers[0] = (int) (Math.random() * 100);
-                numbers[1] = (int) (Math.random() * 100);
+                numbers[0] = (int) (Math.random() * rangeOfNumbers1);
+                numbers[1] = (int) (Math.random() * rangeOfNumbers1);
             }
 
             questions[i] = "Question: " + numbers[0] + " " + operation + " " + numbers[1];
