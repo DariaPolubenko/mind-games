5dda3b914c010354f158759440278815ff005de5
Daria Polubenko <daria.polubenko@gmail.com>
2024-03-09 00:39:48 +0700

    update Even.java

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -3,26 +3,40 @@ package hexlet.code.games;
 import hexlet.code.Engine;
 
 public class Even {
+    private static final int rounds = 3;
+    private static final int length = rounds;
+
+    private static final int rangeOfNumbers = 100;
+    private static final int parityСheck = 2;
+
+
     public static void playEven() {
 
         var greeting = "Answer 'yes' if the number is even, otherwise answer 'no'.";
-
-        final int length = 3;
         String[] questions = new String[length];
         String[] results = new String[length];
 
-        final int rounds = 3;
         for (var i = 0; i < rounds; i++) {
 
-            final int rangeOfNumbers = 100;
-            var number = (int) (Math.random() * rangeOfNumbers);
+            var number = getNumber(rangeOfNumbers);
             questions[i] = "Question: " + number;
-
-            final int integrityTest = 2;
-            int even = number % integrityTest;
-            results[i] = even == 0 ? "yes" : "no";
+            results[i] = getResult(number);
         }
 
         Engine.getEngine(greeting, questions, results);
     }
+
+    public static int getNumber(int rangeOfNumbers) {
+        // от [1 до 100]
+        var number = (int) (Math.random() * rangeOfNumbers + 1);
+        return number;
+    }
+
+    public static String getResult(int number) {
+        var even = number % parityСheck;
+        var result = even == 0 ? "yes" : "no";
+
+        return result;
+    }
+
 }
