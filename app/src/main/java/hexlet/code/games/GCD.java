c7c09c654a4fd40b4874253e902c56418739cb18
Daria Polubenko <daria.polubenko@gmail.com>
2024-03-09 04:48:35 +0700

    update GCD.java

--- app/src/main/java/hexlet/code/games/GCD.java
+++ app/src/main/java/hexlet/code/games/GCD.java
@@ -3,19 +3,19 @@ package hexlet.code.games;
 import hexlet.code.Engine;
 
 public class GCD {
-    private static final int rounds = 3;
-    private static final int numberOfDigits = 2;
-    private static final int minNumber = 2;
-    private static final int maxNumber = 10;
+    private static final int ROUNDS = 3;
+    private static final int NUMBER_OF_DIGITS = 2;
+    private static final int MIN_NUMBER = 2;
+    private static final int MAX_NUMBER = 10;
 
 
     public static void playGCD() {
 
         var greeting = "Find the greatest common divisor of given numbers.";
-        String[] questions = new String[rounds];
-        String[] results = new String[rounds];
+        String[] questions = new String[ROUNDS];
+        String[] results = new String[ROUNDS];
 
-        for (var i = 0; i < rounds; i++) {
+        for (var i = 0; i < ROUNDS; i++) {
 
             int[] numbers = getNumbers();
             questions[i] = "Question: " + numbers[0] + " " + numbers[1];
@@ -25,16 +25,16 @@ public class GCD {
     }
 
     public static int[] getNumbers() {
-        int[] numbers = new int[numberOfDigits];
+        int[] numbers = new int[NUMBER_OF_DIGITS];
 
         do {
             // от [1 до 10]
-            numbers[0] = (int) (Math.random() * maxNumber + 1);
-            numbers[1] = (int) (Math.random() * maxNumber + 1);
+            numbers[0] = (int) (Math.random() * MAX_NUMBER + 1);
+            numbers[1] = (int) (Math.random() * MAX_NUMBER + 1);
         } while (numbers[0] == numbers[1]);
 
         // от [2 до 10]
-        int multiplier =  (int) (Math.random() * (maxNumber - minNumber + 1) + minNumber);
+        int multiplier =  (int) (Math.random() * (MAX_NUMBER - MIN_NUMBER + 1) + MIN_NUMBER);
 
         numbers[0] *= multiplier;
         numbers[1] *= multiplier;
