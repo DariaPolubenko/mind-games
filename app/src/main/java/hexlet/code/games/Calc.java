67cdd4e6dea058273f237c7b54aad8ab51c3622d
Daria Polubenko <daria.polubenko@gmail.com>
2024-03-09 04:47:10 +0700

    update Calc.java

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -3,21 +3,21 @@ package hexlet.code.games;
 import hexlet.code.Engine;
 
 public class Calc {
-    private static final int rounds = 3;
-    private static final int numberOfDigits = 2;
-    private static final int minElement = 0;
-    private  static final int maxElement = 2;
-    private  static final int rangeOfNumbers1 = 10;
-    private  static final int rangeOfNumbers2 = 100;
+    private static final int ROUNDS = 3;
+    private static final int NUMBER_OF_DIGITS = 2;
+    private static final int MIN_ELEMENT = 0;
+    private  static final int MAX_ELEMENT = 2;
+    private  static final int RANGE_OF_NUMBERS1 = 10;
+    private  static final int RANGE_OF_NUMBERS2 = 100;
 
 
     public static void playCalc() {
 
         var greeting = "What is the result of the expression?";
-        String[] questions = new String[rounds];
-        String[] results = new String[rounds];
+        String[] questions = new String[ROUNDS];
+        String[] results = new String[ROUNDS];
 
-        for (var i = 0; i < rounds; i++) {
+        for (var i = 0; i < ROUNDS; i++) {
 
             var operation = getOperation();
             int[] numbers = getNumbers(operation);
@@ -32,23 +32,23 @@ public class Calc {
 
         String[] operations = {"+", "-", "*"};
         // присваиваем знечение от [0 до 2]
-        int randomOperation =  (int) (Math.random() * (maxElement - minElement + 1) + minElement);
+        int randomOperation =  (int) (Math.random() * (MAX_ELEMENT - MIN_ELEMENT + 1) + MIN_ELEMENT);
 
         return operations[randomOperation];
     }
 
     public static int[] getNumbers(String operation) {
 
-        int[] numbers = new int[numberOfDigits];
+        int[] numbers = new int[NUMBER_OF_DIGITS];
 
         if (operation.equals("*")) {
             // присваиваем numbers[0] значения от [1 до 10]
-            numbers[0] = (int) (Math.random() * rangeOfNumbers1 + 1);
-            numbers[1] = (int) (Math.random() * rangeOfNumbers2 + 1);
+            numbers[0] = (int) (Math.random() * RANGE_OF_NUMBERS1 + 1);
+            numbers[1] = (int) (Math.random() * RANGE_OF_NUMBERS2 + 1);
         } else {
             // присваиваем numbers[0] значения от [1 до 100]
-            numbers[0] = (int) (Math.random() * rangeOfNumbers2 + 1);
-            numbers[1] = (int) (Math.random() * rangeOfNumbers2 + 1);
+            numbers[0] = (int) (Math.random() * RANGE_OF_NUMBERS2 + 1);
+            numbers[1] = (int) (Math.random() * RANGE_OF_NUMBERS2 + 1);
         }
         return numbers;
     }
