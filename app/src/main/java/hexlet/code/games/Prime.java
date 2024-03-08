ef9685fe79ab977f32953b5aa3ce448c029c2b25
Daria Polubenko <daria.polubenko@gmail.com>
2024-03-09 01:40:42 +0700

    update Prime.java

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -3,38 +3,43 @@ package hexlet.code.games;
 import hexlet.code.Engine;
 
 public class Prime {
+    private static final int rounds = 3;
+    private static final int length = rounds;
+    private static final int rangeOfNumbers = 100;
+
     public static void playPrime() {
 
         var greeting = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
-
-        final int length = 3;
         String[] questions = new String[length];
         String[] results = new String[length];
 
-        final int rounds = 3;
         for (var i = 0; i < rounds; i++) {
 
-            int number;
-            final int rangeOfNumbers = 100;
-            do {
-                number = (int) (Math.random() * rangeOfNumbers);
-            } while (number == 0);
-
+            int number = getNumber();
             questions[i] = "Question: " + number;
-
-            boolean simpleNumber = true;
-            if (number > 2) {
-                for (var j = 2; j < number; j++) {
-                    if (number % j == 0) {
-                        simpleNumber = false;
-                        break;
-                    }
-                }
-            }
-
-            results[i] = simpleNumber ? "yes" : "no";
+            results[i] = getResult(number);
         }
 
         Engine.getEngine(greeting, questions, results);
+    }
+
+    public static int getNumber() {
+        // от [1 до 100]
+        int number = (int) (Math.random() * rangeOfNumbers + 1);
+        return number;
+    }
+
+    public static String getResult(int number) {
+        boolean simpleNumber = true;
+
+        if (number > 2) {
+            for (var j = 2; j < number; j++) {
+                if (number % j == 0) {
+                    simpleNumber = false;
+                    break;
+                }
+            }
+        }
+        return simpleNumber ? "yes" : "no";
     }
 }
