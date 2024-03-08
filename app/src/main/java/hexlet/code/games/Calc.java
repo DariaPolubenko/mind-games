8b6e43e66fe5d4fd1758b1b7c24bbdd43271b71d
Daria Polubenko <daria.polubenko@gmail.com>
2024-03-09 00:20:10 +0700

    update Calc.java

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -3,57 +3,77 @@ package hexlet.code.games;
 import hexlet.code.Engine;
 
 public class Calc {
+    private static final int length = 3;
+    private static final int rounds = 3;
+    private static final int numberOfDigits = 2;
+    private static final int minElement = 0;
+    private  static final int maxElement = 2;
+    private  static final int rangeOfNumbers1 = 10;
+    private  static final int rangeOfNumbers2 = 100;
+
+
     public static void playCalc() {
 
         var greeting = "What is the result of the expression?";
-
-        final int length = 3;
         String[] questions = new String[length];
-        String[] resultStrings = new String[length];
+        String[] results = new String[length];
 
-        final int rounds = 3;
         for (var i = 0; i < rounds; i++) {
 
-            final int numberOfDigits = 2;
-            int[] numbers = new int[numberOfDigits];
-            String[] operations = {"+", "-", "*"};
-            int result = 0;
-
-            final int minElement = 0;
-            final int maxElement = 2;
-            int randomOperation = minElement + (int) (Math.random() * (maxElement - minElement + 1));
-            var operation = operations[randomOperation];
-
-            final int rangeOfNumbers1 = 100;
-            final int rangeOfNumbers2 = 10;
-
-            if (operation.equals("*")) {
-                numbers[0] = (int) (Math.random() * rangeOfNumbers2);
-                numbers[1] = (int) (Math.random() * rangeOfNumbers1);
-            } else {
-                numbers[0] = (int) (Math.random() * rangeOfNumbers1);
-                numbers[1] = (int) (Math.random() * rangeOfNumbers1);
-            }
+            var operation = getOperation();
+            int[] numbers = getNumbers(numberOfDigits, operation);
 
             questions[i] = "Question: " + numbers[0] + " " + operation + " " + numbers[1];
-
-            switch (operation) {
-                case "+":
-                    result = numbers[0] + numbers[1];
-                    break;
-                case "-":
-                    result = numbers[0] - numbers[1];
-                    break;
-                case "*":
-                    result = numbers[0] * numbers[1];
-                    break;
-                default:
-                    System.out.println("Error");
-            }
-
-            resultStrings[i] = String.valueOf(result);
+            results[i] = getResult(numbers, operation);
         }
 
-        Engine.getEngine(greeting, questions, resultStrings);
+        Engine.getEngine(greeting, questions, results);
+    }
+
+    public static String getOperation() {
+
+        String[] operations = {"+", "-", "*"};
+        // присваиваем знечение от [0 до 2]
+        int randomOperation =  (int) (Math.random() * (maxElement - minElement + 1) + minElement);
+
+        return operations[randomOperation];
+    }
+
+    public static int[] getNumbers(int numberOfDigits, String operation) {
+
+        int[] numbers = new int[numberOfDigits];
+
+        if (operation.equals("*")) {
+            // присваиваем numbers[0] значения от [1 до 10]
+            numbers[0] = (int) (Math.random() * rangeOfNumbers1 + 1);
+            numbers[1] = (int) (Math.random() * rangeOfNumbers2 + 1);
+        } else {
+            // присваиваем numbers[0] значения от [1 до 100]
+            numbers[0] = (int) (Math.random() * rangeOfNumbers2 + 1);
+            numbers[1] = (int) (Math.random() * rangeOfNumbers2 + 1);
+        }
+        return numbers;
+    }
+
+    public static String getResult(int[] numbers, String operation) {
+        int result = 0;
+
+        switch (operation) {
+            case "+":
+                result = numbers[0] + numbers[1];
+                break;
+            case "-":
+                result = numbers[0] - numbers[1];
+                break;
+            case "*":
+                result = numbers[0] * numbers[1];
+                break;
+            default:
+                System.out.println("Error");
+                break;
+        }
+        var resultString = String.valueOf(result);
+
+        return resultString;
     }
 }
