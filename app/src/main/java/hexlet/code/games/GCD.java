fd784b7722406e39afad4368fd1e6bab77fc3feb
Daria Polubenko <daria.polubenko@gmail.com>
2024-03-15 01:48:22 +0700

    update GCD.java

--- app/src/main/java/hexlet/code/games/GCD.java
+++ app/src/main/java/hexlet/code/games/GCD.java
@@ -7,54 +7,52 @@ public class GCD {
     private static final int NUMBER_OF_DIGITS = 2;
     private static final int MIN_NUMBER = 1;
     private static final int MAX_NUMBER = 10;
-
+    private static String greeting = "Find the greatest common divisor of given numbers.";
 
     public static void playGCD() {
 
-        var greeting = "Find the greatest common divisor of given numbers.";
         String[] questions = new String[Engine.ROUNDS];
         String[] results = new String[Engine.ROUNDS];
 
         for (var i = 0; i < Engine.ROUNDS; i++) {
-
             int[] numbers = getNumbers();
-            questions[i] = "Question: " + numbers[0] + " " + numbers[1];
-            results[i] = getResult(numbers);
+            var firstNumber = numbers[0];
+            var secondNumber = numbers[1];
+
+            questions[i] = "Question: " + firstNumber + " " + secondNumber;
+
+            var result = getResult(firstNumber, secondNumber);
+            results[i] = String.valueOf(result);
         }
         Engine.getEngine(greeting, questions, results);
     }
 
     public static int[] getNumbers() {
-        int[] numbers = new int[NUMBER_OF_DIGITS];
 
+        int[] numbers = new int[NUMBER_OF_DIGITS];
         do {
-            // от [1 до 10]
-            numbers[0] = Utils.getRandomInt(MIN_NUMBER, MAX_NUMBER);
+            numbers[0] = Utils.getRandomInt(MIN_NUMBER, MAX_NUMBER); // от [1 до 10]
             numbers[1] = Utils.getRandomInt(MIN_NUMBER, MAX_NUMBER);
         } while (numbers[0] == numbers[1]);
 
-        // от [2 до 10]
-        int multiplier = Utils.getRandomInt(MIN_NUMBER + 1, MAX_NUMBER);
-
+        int multiplier = Utils.getRandomInt(MIN_NUMBER + 1, MAX_NUMBER); // от [2 до 10]
         numbers[0] *= multiplier;
         numbers[1] *= multiplier;
 
         return numbers;
     }
 
-    public static String getResult(int[] numbers) {
+    public static int getResult(int first, int second) {
 
-        while (numbers[0] != 0 && numbers[1] != 0) {
-            if (numbers[0] > numbers[1]) {
-                numbers[0] = numbers[0] % numbers[1];
+        while (first != 0 && second != 0) {
+            if (first > second) {
+                first = first % second;
             } else {
-                numbers[1] = numbers[1] % numbers[0];
+                second = second % first;
             }
         }
-        var result = numbers[0] + numbers[1];
-        var resultString = String.valueOf(result);
-
-        return resultString;
+        var result = first + second;
+        return result;
     }
 
 }
