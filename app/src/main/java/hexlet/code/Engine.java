f75f9eb6edff5d65cb287644c9eed4f257713ac5
Daria Polubenko <daria.polubenko@gmail.com>
2024-03-20 04:32:14 +0700

    update Engine.java

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Engine {
     public static final int ROUNDS = 3;
-    public static void run(String greeting, String[] questions, String[] results) {
+    public static void run(String greeting, String[][] data) {
 
         System.out.println("Welcome to the Brain Games!\n"
                 + "May I have your name? ");
@@ -15,18 +15,18 @@ public class Engine {
 
         for (var i = 0; i < ROUNDS; i++) {
 
-            System.out.println(questions[i]);
+            System.out.println(data[i][0]);
 
             Scanner answer = new Scanner(System.in);
             System.out.print("Your answer: ");
             String userAnswer = answer.next();
 
-            if (results[i].equals(userAnswer)) {
+            if (data[i][1].equals(userAnswer)) {
                 System.out.println("Correct!");
-            } else if (!results[i].equals(userAnswer)) {
+            } else if (!data[i][1].equals(userAnswer)) {
                 System.out.println("'" + userAnswer + "'"
                         + " is wrong answer ;(. Correct answer was " + "'"
-                        + results[i] + "'.");
+                        + data[i][1] + "'.");
                 System.out.println("Let's try again, " + userName + "!");
                 return;
             }
