572cf4d2bee2a993b2939dc193555e8b24189e7f
Daria Polubenko <daria.polubenko@gmail.com>
2024-03-27 03:11:21 +0700

    update Prime.java

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -4,7 +4,7 @@ import hexlet.code.Engine;
 import hexlet.code.Utils;
 
 public class Prime {
-    private static final int MIN_NUMBER = 1;
+    private static final int MIN_NUMBER = 0;
     private static final int MAX_NUMBER = 100;
     private static String greeting = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
 
@@ -14,25 +14,23 @@ public class Prime {
 
         for (var i = 0; i < Engine.ROUNDS; i++) {
 
-            int number = Utils.getRandom(MIN_NUMBER, MAX_NUMBER); // [1, 100]
+            int number = Utils.getRandomNumber(MIN_NUMBER, MAX_NUMBER); // [1, 100]
             data[i][0] = "Question: " + number;
 
-            var result = getResult(number);
-            data[i][1] = result ? "yes" : "no";
+            var answer = isPrime(number);
+            data[i][1] = answer ? "yes" : "no";
         }
         Engine.run(greeting, data);
     }
 
-    public static boolean getResult(int number) {
+    public static boolean isPrime(int number) {
         boolean simpleNumber = true;
 
-        if (number == 1) {
+        if (number == 1 || number == 0) {
             simpleNumber = false;
 
         } else if (number > 2) {
-
             for (var i = 2; i < number; i++) {
-
                 if (number % i == 0) {
                     simpleNumber = false;
                     break;
