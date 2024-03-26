fd54360a24b09fb6685393f93bc2955ccff67f71
Daria Polubenko <daria.polubenko@gmail.com>
2024-03-27 03:11:50 +0700

    update Progression.java

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -6,12 +6,11 @@ import hexlet.code.Utils;
 import java.util.Arrays;
 
 public class Progression {
-    private static final int LENGTH = 10;
-    private static final int MIN_NUMBER = 1;
+    private static final int MIN_NUMBER1 = 1;
+    private static final int MIN_NUMBER2 = 5;
     private static final int MAX_FIRST_NUMBER = 10;
     private static final int MAX_NUMBER_PROGRESSION = 20;
     private static String greeting = "What number is missing in the progression?";
-    private static int missingNumber;
 
     public static void playProgression() {
 
@@ -19,36 +18,39 @@ public class Progression {
 
         for (var i = 0; i < Engine.ROUNDS; i++) {
 
-            int[] numbers = getNumbers();
-            missingNumber = Utils.getRandom(0, LENGTH - 1); // от [0 до длина массива)
+            var lengthOfProgression = Utils.getRandomNumber(MIN_NUMBER2, MAX_FIRST_NUMBER); // от [5 до 10]
+            var firstNumberOfProgression = Utils.getRandomNumber(MIN_NUMBER1, MAX_FIRST_NUMBER); // от [1 до 10]
+            var progressionStep = Utils.getRandomNumber(MIN_NUMBER1, MAX_NUMBER_PROGRESSION); // от [1 до 20]
 
-            var result = numbers[missingNumber];
-            data[i][1] = String.valueOf(result);
+            int[] progression = getProgression(lengthOfProgression, firstNumberOfProgression, progressionStep);
 
-            var question = getQuestion(numbers);
+            var missingNumber = Utils.getRandomNumber(0, lengthOfProgression - 1); // от [0 до длина массива)
+            var answer = progression[missingNumber];
+            data[i][1] = String.valueOf(answer);
+
+            var question = getQuestion(progression, missingNumber);
             data[i][0] = "Question: " + question;
         }
         Engine.run(greeting, data);
     }
 
-    public static int[] getNumbers() {
-        int[] numbers = new int[LENGTH];
-        numbers[0] = Utils.getRandom(MIN_NUMBER, MAX_FIRST_NUMBER); // от [1 до 10]
+    public static int[] getProgression(int length, int firstNumber, int step) {
+        int[] numbers = new int[length];
+        numbers[0] = firstNumber;
 
-        int progressionNumber = Utils.getRandom(MIN_NUMBER, MAX_NUMBER_PROGRESSION); // от [1 до 20]
-        for (var i = 1; i < LENGTH; i++) {
-            numbers[i] = numbers[i - 1] + progressionNumber;
+        for (var i = 1; i < length; i++) {
+            numbers[i] = numbers[i - 1] + step;
         }
         return numbers;
     }
 
-    public static String getQuestion(int[] numbers) {
-        String[] numbersString = Arrays.stream(numbers)
+    public static String getQuestion(int[] progression, int missingNumber) {
+        String[] progressionString = Arrays.stream(progression)
                 .mapToObj(String::valueOf)
                 .toArray(String[]::new);
 
-        numbersString[missingNumber] = "..";
-        var question = String.join(" ", numbersString);
+        progressionString[missingNumber] = "..";
+        var question = String.join(" ", progressionString);
         return question;
     }
 }
