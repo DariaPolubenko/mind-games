c61cfb9026dde652332da03786e63cc7895c9720
Daria Polubenko <daria.polubenko@gmail.com>
2024-03-27 03:09:58 +0700

    update Utils.java

--- app/src/main/java/hexlet/code/Utils.java
+++ app/src/main/java/hexlet/code/Utils.java
@@ -1,7 +1,7 @@
 package hexlet.code;
 
 public class Utils {
-    public static int getRandom(int min, int max) {
+    public static int getRandomNumber(int min, int max) {
         var randomNumber = (int) (Math.random() * (max - min + 1) + min);
         return randomNumber;
     }
